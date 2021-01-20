6255408013d6ca21206c0f8c05015563e289e710
demidraksin <greatraksin@icloud.com>
2021-01-20 10:12:25 +0300

    Adding new view for index

--- app.py
+++ app.py
@@ -5,13 +5,22 @@ app = Flask(__name__, instance_relative_config=False)
 app.config.from_object('config.Config')
 
 
-@app.route('/', methods=('GET', 'POST'))
-def contacts():
+@app.route('/')
+def main_page():
+    return render_template('index.html')
+
+
+@app.route('/contact', methods=('GET', 'POST'))
+def contact():
     form = ContactForm()
     if form.validate_on_submit():
         return redirect(url_for('success'))
     return render_template('contact.html', form=form)
 
+
+@app.route('/signup')
+def signup():
+    return 'hellpo'
 
 if __name__ == '__main__':
     app.run(debug=True)
